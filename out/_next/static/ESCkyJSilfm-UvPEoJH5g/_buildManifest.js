daf1e31ae082c04b669cf85ad4c96f6db007e539
CodeLabWithOsman <abutazure@gmail.com>
2025-11-12 07:46:28 +0000

    Add GitHub Actions workflow for proper Pages deployment

--- out/_next/static/ESCkyJSilfm-UvPEoJH5g/_buildManifest.js
+++ out/_next/static/ESCkyJSilfm-UvPEoJH5g/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/bit-attendance//_next/:path+",
-        "destination": "/bit-attendance/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
