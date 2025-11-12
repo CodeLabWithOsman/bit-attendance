2bf1bee6420df1b4cc46091b5795eeee357a66f0
CodeLabWithOsman <abutazure@gmail.com>
2025-11-12 09:38:11 +0000

    Updates

--- _next/static/chunks/132a4cd850265b51.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/about", [
-  "static/chunks/7302ab7bcf79737a.js",
-  "static/chunks/d23789ea24734f35.js",
-  "static/chunks/eefc63b864be81ee.js",
-  "static/chunks/turbopack-1c58a23f653dfe79.js"
-])
