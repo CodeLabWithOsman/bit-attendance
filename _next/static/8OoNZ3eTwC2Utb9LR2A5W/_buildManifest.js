1d46006f3bfc418773df338c8069fa90d23e4074
CodeLabWithOsman <abutazure@gmail.com>
2025-11-12 07:31:26 +0000

    Updates

--- _next/static/8OoNZ3eTwC2Utb9LR2A5W/_buildManifest.js
+++ _next/static/8OoNZ3eTwC2Utb9LR2A5W/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/bit-attendance//_next/:path+",
+        "destination": "/bit-attendance/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
